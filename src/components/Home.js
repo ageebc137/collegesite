726970c63db7eedb9c017b5e098f0d325246b150
Allister <ageebernal@aol.com>
2020-05-13 23:12:30 -0500

    add homepage and navbars

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,39 @@ import React from 'react';
 
 function HomePage() {
     return (
-        <>
+        <> 
+            <header>
+            <section id="brand-bar">
+                <a class="brand-title">Hambridge University</a>
+                <nav class="navbar" >
+                    <ul class="navbar-list navbar-top-list">
+                        <li class="navbar-item">Students</li>
+                        <li class="navbar-item">Faculty &#x26; Staff</li>
+                        <li class="navbar-item">Parents </li>
+                        <li class="navbar-item">Visitors</li>
+                        <li class="navbar-item">Alumni</li>
+                    </ul>
+                </nav>
+            </section>
+            <section>
+                <nav class=""> 
+                    <ul class="navbar-list navbar-bot-list">
+                            <li class="">News</li>
+                            <li class="">Events</li>
+                            <li class="">Academics</li>
+                            <li class="">Research</li>
+                            <li class="">Health Care</li>
+                            <li class="">Campus Life</li>
+                            <li class="">Admission</li>
+                            <li class="">About</li>
+                        </ul>
+                </nav>
+            </section>
+            
+            </header>
+            
         </>
-    )
+    );
 }
 
 export default HomePage;
--- src/router/Router.js
+++ src/router/Router.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
+import Home from '../components/Home';
 
 function RouterComponent() {
     return (
         <Router>
             <Switch>
-                <Route path="/"></Route>
+                <Route path="/" component={Home}></Route>
             </Switch>
         </Router>
     )
