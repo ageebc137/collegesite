195c39302640b9d494a86b7235831004f8357fd6
Allister <ageebernal@aol.com>
2020-05-17 20:53:00 -0500

    add line hover effects

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,38 +4,38 @@ function HomePage() {
     return (
         <> 
             <header>
-            <section id="brand-bar">
-                <a className="brand-title">Hambridge University</a>
-                <nav className="navbar" >
-                    <ul className="navbar-list navbar-top-list">
-                        <li className="navbar-item">Students</li>
-                        <li className="navbar-item">Faculty &#x26; Staff</li>
-                        <li className="navbar-item">Parents </li>
-                        <li className="navbar-item">Visitors</li>
-                        <li className="navbar-item">Alumni</li>
-                    </ul>
-                </nav>
-            </section>
-            <section>
-                <nav className=""> 
-                    <ul className="navbar-list navbar-bot-list">
-                            <li className="">News</li>
-                            <li className="">Events</li>
-                            <li className="">Academics</li>
-                            <li className="">Research</li>
-                            <li className="">Health Care</li>
-                            <li className="">Campus Life</li>
-                            <li className="">Admission</li>
-                            <li className="">About</li>
+                <section id="brand-bar">
+                    <a className="brand-title">Hambridge University</a>
+                    <nav className="navbar" >
+                        <ul className="navbar-list navbar-top-list">
+                            <li className="navbar-item">Students</li>
+                            <li className="navbar-item">Faculty &#x26; Staff</li>
+                            <li className="navbar-item">Parents </li>
+                            <li className="navbar-item">Visitors</li>
+                            <li className="navbar-item">Alumni</li>
                         </ul>
-                </nav>
-            </section>
-            <section id="center-panel">
-                <h1 className="brand-title">Hambridge</h1>
-            </section>
-            {/* <section>
-                <p>Explore Hambridge</p>
-            </section> */}
+                    </nav>
+                </section>
+                <section id="sub-bar">
+                    <nav> 
+                        <ul className="navbar-list navbar-bot-list">
+                                <li className="navbar-item navbar-item-bottom">News</li>
+                                <li className="navbar-item navbar-item-bottom">Events</li>
+                                <li className="navbar-item navbar-item-bottom">Academics</li>
+                                <li className="navbar-item navbar-item-bottom">Research</li>
+                                <li className="navbar-item navbar-item-bottom">Health Care</li>
+                                <li className="navbar-item navbar-item-bottom">Campus Life</li>
+                                <li className="navbar-item navbar-item-bottom">Admission</li>
+                                <li className="navbar-item navbar-item-bottom">About</li>
+                        </ul>
+                    </nav>
+                </section>
+                <section id="center-panel">
+                    <h1 className="brand-title">Hambridge</h1>
+                </section>
+                <section id="explore-banner">
+                    <p>Explore Hambridge</p>
+                </section>
             </header>
             
         </>
