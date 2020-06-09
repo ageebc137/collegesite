d4379e64a67cdb9b8d56f96fb3135eeb0bb77baa
Allister <ageebernal@aol.com>
2020-06-08 22:53:18 -0500

    fix the layout of the event section

--- src/components/Home.js
+++ src/components/Home.js
@@ -90,6 +90,29 @@ function HomePage() {
                             <p>to make self-driving cars safer</p>
                         </div>
                     </div>
+                    <button> More Hambridge News</button>
+                </section>
+                <section id="hambridge-events">
+                    <h1>Hambridge Events</h1>
+                    <p>What's happening on campus?</p>
+                    <div id="latest-events">
+                        <div class="event-widget">
+                            <p>A Virtual Workshop On </p>
+                            <p>Economic Growth Amid Corona Crisis</p>
+                        </div>
+                        <div class="event-widget"> 
+                            <p>Digital Grid Virtual Workshop : </p>
+                            <p>Integrating Customer Resources</p>
+                            <p>- U.S. Utility Panel Session</p>
+                        </div>
+                        <div class="event-widget">
+                            <p>Fireside Chat with Janet Moore</p>
+                        </div>
+                        <div class="event-widget">
+                            <p>Hambridge University Choir</p>
+                            <p>Summer Musical Concert </p>
+                        </div>
+                    </div>
                 </section>
             </main>
             
