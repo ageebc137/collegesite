3baeaa4640ad7d94961a00ee74ad8095cf207c91
Allister <ageebernal@aol.com>
2020-06-11 22:13:25 -0500

    add eventscontainer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NewsContainer from './Home/NewsContainer';
+import EventsContainer from './Home/EventsContainer';
 
 function HomePage() {
     return (
@@ -41,28 +42,7 @@ function HomePage() {
             </header>
             <main>
                 <NewsContainer></NewsContainer>
-                <section id="hambridge-events">
-                    <h1>Hambridge Events</h1>
-                    <p>What's happening on campus?</p>
-                    <div id="latest-events">
-                        <div class="event-widget">
-                            <p>A Virtual Workshop On </p>
-                            <p>Economic Growth Amid Corona</p>
-                        </div>
-                        <div class="event-widget"> 
-                            <p>Digital Grid Virtual Workshop : </p>
-                            <p>Integrating Customer Resources</p>
-                            <p>- U.S. Utility Panel Session</p>
-                        </div>
-                        <div class="event-widget">
-                            <p>Fireside Chat with Janet Moore</p>
-                        </div>
-                        <div class="event-widget">
-                            <p>Hambridge University Choir</p>
-                            <p>Summer Musical Concert </p>
-                        </div>
-                    </div>
-                </section>
+                <EventsContainer></EventsContainer>
             </main>
             
         </>
---
+++ src/components/Home/EventsContainer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function EventsContainer() {
+    return (
+        <section id="hambridge-events">
+        <h1>Hambridge Events</h1>
+        <p>What's happening on campus?</p>
+        <div id="latest-events">
+            <div class="event-widget">
+                <p>A Virtual Workshop On </p>
+                <p>Economic Growth Amid Corona</p>
+            </div>
+            <div class="event-widget"> 
+                <p>Digital Grid Virtual Workshop : </p>
+                <p>Integrating Customer Resources</p>
+                <p>- U.S. Utility Panel Session</p>
+            </div>
+            <div class="event-widget">
+                <p>Fireside Chat with Janet Moore</p>
+            </div>
+            <div class="event-widget">
+                <p>Hambridge University Choir</p>
+                <p>Summer Musical Concert </p>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default EventsContainer;
