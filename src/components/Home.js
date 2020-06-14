13a508dc8fd418ddcd9ab125e6f0f39cb393802b
Allister <ageebernal@aol.com>
2020-06-13 23:46:58 -0500

    containerize bot bar menu

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,26 +2,14 @@ import React from 'react';
 import NewsContainer from './Home/NewsContainer';
 import EventsContainer from './Home/EventsContainer';
 import TopBarMenu from './Home/TopBarMenu';
+import BotBarMenu from './Home/BotBarMenu';
 
 function HomePage() {
     return (
         <> 
             <header>
                 <TopBarMenu></TopBarMenu>
-                <section id="sub-bar">
-                    <nav> 
-                        <ul className="navbar-list navbar-bot-list">
-                                <li className="navbar-item navbar-item-bottom">News</li>
-                                <li className="navbar-item navbar-item-bottom">Events</li>
-                                <li className="navbar-item navbar-item-bottom">Academics</li>
-                                <li className="navbar-item navbar-item-bottom">Research</li>
-                                <li className="navbar-item navbar-item-bottom">Health Care</li>
-                                <li className="navbar-item navbar-item-bottom">Campus Life</li>
-                                <li className="navbar-item navbar-item-bottom">Admission</li>
-                                <li className="navbar-item navbar-item-bottom">About</li>
-                        </ul>
-                    </nav>
-                </section>
+                <BotBarMenu></BotBarMenu>
                 <section id="center-panel">
                     <h1 className="brand-title">Hambridge</h1>
                 </section>
---
+++ src/components/Home/BotBarMenu.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function BotBarMenu() {
+    return (
+        <section id="sub-bar">
+        <nav> 
+            <ul className="navbar-list navbar-bot-list">
+                    <li className="navbar-item navbar-item-bottom">News</li>
+                    <li className="navbar-item navbar-item-bottom">Events</li>
+                    <li className="navbar-item navbar-item-bottom">Academics</li>
+                    <li className="navbar-item navbar-item-bottom">Research</li>
+                    <li className="navbar-item navbar-item-bottom">Health Care</li>
+                    <li className="navbar-item navbar-item-bottom">Campus Life</li>
+                    <li className="navbar-item navbar-item-bottom">Admission</li>
+                    <li className="navbar-item navbar-item-bottom">About</li>
+            </ul>
+        </nav>
+    </section>
+    )
+}
+
+export default BotBarMenu;
