921b4ba390e7aa0ba8816f20b3b30c70c51f32bb
Allister <ageebernal@aol.com>
2020-08-05 20:35:55 -0500

    changes to healthcare

--- src/components/Home/HealthcareContainer.js
+++ src/components/Home/HealthcareContainer.js
@@ -3,6 +3,8 @@ import React from 'react';
 function HealthcareContainer() {
     return (
         <section>
+            <h1>Health Care</h1>
+            <p>Caring for people and advancing human health through innovative research, education and health care</p>
         </section>
     )
 }
